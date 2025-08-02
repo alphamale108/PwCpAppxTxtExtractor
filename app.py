17b94c995665ffd5907384453b124fdad0e6f805
alphamale108 <136615536+alphamale108@users.noreply.github.com>
2025-08-02 15:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,9 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Bot credentials from environment variables (Render compatible)
-API_ID = int(os.environ.get("20081897"))
-API_HASH = os.environ.get("8051dfd6c39c07e3eb56d58ef7f9f15f")
-BOT_TOKEN = os.environ.get("8437726474:AAE6-hpZIu_D3KuuwcTW0q4cmLKEbnwj-Bg")
-
+API_ID = int(os.environ.get("API_ID", "20081897"))
+API_HASH = os.environ.get("API_HASH", "8051dfd6c39c07e3eb56d58ef7f9f15f")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8437726474:AAE6-hpZIu_D3KuuwcTW0q4cmLKEbnwj-Bg")
 
 # Validate environment variables
 if not all([API_ID, API_HASH, BOT_TOKEN]):
